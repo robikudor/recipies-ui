c1931a2cc95e46da74ce445e88633a508be713b6
Robi Kudor <robert.kudor@wolfpack-digital.com>
2022-02-09 10:16:50 +0200

    Add heroku base path

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
-export const baseApiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/api';
+// export const baseApiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://robi-recipes-api.herokuapp.com';
+export const baseApiUrl = 'https://robi-recipes-api.herokuapp.com';

dca4a23a749f81f1bb278003e126231a0fd85cf1
Robi Kudor <robert.kudor@wolfpack-digital.com>
2022-02-14 17:03:24 +0200

    Set correct base path

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const baseApiUrl = 'http://localhost:3000';
-// export const baseApiUrl = 'https://robi-recipes-api.herokuapp.com';
+// export const baseApiUrl = 'http://localhost:3000';
+export const baseApiUrl = 'https://robi-recipes-api.herokuapp.com';
